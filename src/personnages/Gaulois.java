124f857369f522fb588960fc1cc746c1d186c8d2
NicolasGlouxGloux <nicolas.gloux@univ-tlse3.fr>
2024-01-17 09:24:15 +0100

    TP1 methode parler gaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -7,4 +7,17 @@ public class Gaulois {
 	public String getNom() {
 		return nom;
 	}
+	
+	public Gaulois (String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public void parler (String texte) {
+		System.out.println("le gaulois " + nom + " : " + texte);
+	}
+	
+//	public static void main(String[] args) {
+//		Gaulois asterix = new Gaulois("Astérix", 8);
+//		System.out.println(asterix.getNom());
+//	}
 }
--- src/personnages/TestPersonnages.java
+++ src/personnages/TestPersonnages.java
@@ -1,11 +1,8 @@
-package test_fonctionnel;
+package personnages;
 
-import personnages.Gaulois;
-
-public class TestGaulois {
+public class TestPersonnages {
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix", 8);
 		System.out.println(asterix.getNom());
-		asterix.parler("Bonjour à tous");
 	}
 }
